a3c89def73f8fe7e5b1edd4596c28e8227db8f9f
Sameer Saxena <sameerdsaxena@gmail.com>
2022-11-16 17:43:47 -0500

    Addcourse uses dropdown of courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+courses = [{:name => 'Principles of Economics', :course => 'ECON UN1105 Principles of Economics', :prof => 'Sunil Gulati'},
+           {:name => 'Advanced Programming', :course => 'COMS 3157 Advanced Programming', :prof => 'Jae Woo Lee'},
+           {:name => 'Discrete Math', :course => 'COMS 3203 Discrete Math', :prof => 'Ansaf Salleb-Aboussai'}
+  	 ]
+
+courses.each do |course|
+    Course.create!(course)
+end
