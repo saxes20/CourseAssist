47dad08c17d6d9ad8ce74c08add948cac737d077
agh2172 <agh2172@columnbia.edu>
2022-11-16 20:39:35 -0500

    Rspec test for addcourses

--- spec/controllers/addcourses_controller_spec.rb
+++ spec/controllers/addcourses_controller_spec.rb
@@ -7,4 +7,11 @@ describe AddcoursesController do
         input_uni = assigns(:uni)
         expect(input_uni).to eq('ss6167')
     end
+    before do
+        post :create, params: {"addcourse"=>{"course"=>"ECON UN1105 Principles of Economics", "semester"=>"Fall", "year"=>"2018"}}
+    end
+    it 'should assign courseyear to year' do
+        year = assigns(:courseyear)
+        expect(year).to eq(2018)
+    end
 end
