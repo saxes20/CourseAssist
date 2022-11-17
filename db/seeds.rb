fb244961a4821b3cb20084e62e675ffe7ad75e7f
Sameer Saxena <sameerdsaxena@gmail.com>
2022-11-16 23:29:47 -0500

    Added data to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,15 @@ courses = [{:name => 'Principles of Economics', :course => 'ECON UN1105 Principl
            {:name => 'Discrete Math', :course => 'COMS 3203 Discrete Math', :prof => 'Ansaf Salleb-Aboussai', :preReqs => 'Any introductory course in computer programming', :description => 'Logic and formal proofs, sequences and summation, mathematical induction, binomial coefficients, elements of finite probability, recurrence relations, equivalence relations and partial orderings, and topics in graph theory (including isomorphism, traversability, planarity, and colorings).'},
            {:name => 'Engineering SaaS', :course => 'COMS 4152 Engineering SaaS', :prof => 'Junfeng Yang', :preReqs => '', :description => ''},
            {:name => 'Intermediate Microeconomics', :course => 'ECON UN3211 Intermediate Microeconomics'}
+           {:course => 'ECON UN3213 Intermediate Macroeconomics'},
+           {:course => 'ECON UN3412 Intro to Econometrics'},
+           {:course => 'COMS W3134 Data Structures in Java'}, 
+           {:course => 'COMS W1004 Intro to Programming in Java'}, 
+           {:course => 'COMS W3261 Computer Science Theory'},
+           {:course => 'COMS W3827 Fundamentals of Computer Systems'},
+           {:course => 'ECON UN2105 The American Economy'},
+           {:course => 'ECON UN2257 Global Economy'},
+           {:course => 'ECON UN3025 Financial Economics'},
           ]
 
 school_reqs = [{:school => 'SEAS', :course => 'ECON UN1105 Principles of Economics'},
@@ -21,11 +30,28 @@ school_reqs = [{:school => 'SEAS', :course => 'ECON UN1105 Principles of Economi
                {:school => 'CC', :course => 'HUMA UN1121 Masterpieces of Western Art'},
                {:school => 'CC', :course => 'HUMA UN1123 Masterpieces of Western Music'}]
 
-major_reqs = [{:subject => 'Computer Science', :course => 'COMS 3157 Advanced Programming'},
-              {:subject => 'Computer Science', :course => 'COMS 3203 Discrete Math'}]
+major_reqs = [{:subject => 'Economics', :course => 'ECON UN1105 Principles of Economics'},
+            {:subject => 'Economics', :course => 'ECON UN3211 Intermediate Microeconomics'}, 
+            {:subject => 'Economics', :course => 'ECON UN3213 Intermediate Macroeconomics'},
+            {:subject => 'Economics', :course => 'ECON UN3412 Intro to Econometrics'},
+            {:subject => 'Economics', :course => 'ECON UN2105 The American Economy'},
+            {:subject => 'Economics', :course => 'ECON UN2257 Global Economy'},
+            {:subject => 'Economics', :course => 'ECON UN3025 Financial Economics'}
+            {:subject => 'Computer Science', :course => 'COMS W1004 Intro to Programming in Java'}, 
+            {:subject => 'Computer Science', :course => 'COMS W3134 Data Structures in Java'}, 
+            {:subject => 'Computer Science', :course => 'COMS W3157 Advanced Programming'}, 
+            {:subject => 'Computer Science', :course => 'COMS W3203 Discrete Math'},
+            {:subject => 'Computer Science', :course => 'COMS W3261 Computer Science Theory'},
+            {:subject => 'Computer Science', :course => 'COMS W3827 Fundamentals of Computer Systems'}]
 
 minor_reqs = [{:subject => 'Economics', :course => 'ECON UN1105 Principles of Economics'},
-              {:subject => 'Economics', :course => 'ECON UN3211 Intermediate Microeconomics'}]
+              {:subject => 'Economics', :course => 'ECON UN3211 Intermediate Microeconomics'}, 
+              {:subject => 'Economics', :course => 'ECON UN3213 Intermediate Macroeconomics'},
+              {:subject => 'Economics', :course => 'ECON UN3412 Intro to Econometrics'},
+              {:subject => 'Computer Science', :course => 'COMS W1004 Intro to Programming in Java'}, 
+              {:subject => 'Computer Science', :course => 'COMS W3134 Data Structures in Java'}, 
+              {:subject => 'Computer Science', :course => 'COMS W3157 Advanced Programming'}, 
+              {:subject => 'Computer Science', :course => 'COMS W3203 Discrete Math'}]
 
 courses.each do |course|
     Course.create!(course)
