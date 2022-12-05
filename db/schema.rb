e33b4fe10e7b7aaf0bb008347dbf0c6905870940
Sameer Saxena <sameerdsaxena@gmail.com>
2022-12-05 14:04:44 -0500

    Add course functionality on dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_003435) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_183323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_003435) do
 
   create_table "schoolreqs", force: :cascade do |t|
     t.string "school"
+    t.string "course"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "studentschedules", force: :cascade do |t|
+    t.string "uni"
     t.string "course"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
