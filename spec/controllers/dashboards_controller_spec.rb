6e9d9314cb956ed1162c0ce6e6a1952a59d5f525
agh2172 <agh2172@columnbia.edu>
2022-12-19 14:49:17 -0500

    Rspec Tests updated

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -22,7 +22,8 @@ describe DashboardsController do
                             year: 2023,
                             school: 'CC',
                             major: 'Cycling',
-                            minor: 'None')
+                            minor: 'None',
+                            password: '123456')
         user4 = User.create!(first: 'Lucas',
                             last: 'Cremers',
                             uni: 'lc1902',
@@ -40,12 +41,12 @@ describe DashboardsController do
         courses = [{:name => 'Principles of Economics', :course => 'ECON UN1105 Principles of Economics', :prof => 'Sunil Gulati', :preReqs => 'None', :description => 'How a market economy determines the relative prices of goods, factors of production, and the allocation of resources and the circumstances under which it does it efficiently. Why such an economy has fluctuations and how they may becontrolled.'},
                             {:name => 'The Art of Engineering', :course => 'ENGI E1102 The Art of Engineering', :prof => 'David Vallancourt'},
                             {:name => 'Masterpieces of Western Art', :course => 'HUMA UN1121 Masterpieces of Western Art'},
-                            {:name => 'Masterpieces of Western Music', :course => 'HUMA UN1123 Masterpieces of Western Music'},
+                            {:name => 'Masterpieces of Western Music', :course => 'HUMA UN1123 Masterpieces of Western Music', :time => 'MWF'},
                             {:name => 'Advanced Programming', :course => 'COMS 3157 Advanced Programming', :prof => 'Jae Woo Lee', :preReqs => '(COMS W3134) or (COMS W3137)', :description => 'C programming language and Unix systems programming. Also covers Git, Make, TCP/IP networking basics, C++ fundamentals.'},
                             {:name => 'Discrete Math', :course => 'COMS 3203 Discrete Math', :prof => 'Ansaf Salleb-Aboussai', :preReqs => 'Any introductory course in computer programming', :description => 'Logic and formal proofs, sequences and summation, mathematical induction, binomial coefficients, elements of finite probability, recurrence relations, equivalence relations and partial orderings, and topics in graph theory (including isomorphism, traversability, planarity, and colorings).'},
-                            {:name => 'Engineering SaaS', :course => 'COMS 4152 Engineering SaaS', :prof => 'Junfeng Yang', :preReqs => '', :description => ''},
+                            {:name => 'Engineering SaaS', :course => 'COMS 4152 Engineering SaaS', :prof => 'Junfeng Yang', :preReqs => '', :description => '', :time => 'TR'},
                             {:name => 'Intermediate Microeconomics', :course => 'ECON UN3211 Intermediate Microeconomics'}
-                          ]                         
+                          ]
 
         school_reqs = [{:school => 'SEAS', :course => 'ECON UN1105 Principles of Economics'},
                             {:school => 'SEAS', :course => 'ENGI E1102 The Art of Engineering'},
@@ -62,7 +63,7 @@ describe DashboardsController do
                             {:subject => 'Economics', :course => 'ECON UN3211 Intermediate Microeconomics'},
                           {:subject => 'Computer Science', :course => 'COMS 3157 Advanced Programming'}]
 
-        schedules = [{:uni => 'agh2172', :course => 'COMS 4152 Engineering SaaS'}]
+        schedules = [{:uni => 'agh2172', :course => 'COMS 4152 Engineering SaaS'}, {:uni => 'agh2172', :course => 'HUMA UN1123 Masterpieces of Western Music'}]
 
         courses.each do |course|
           Course.create!(course)
@@ -164,6 +165,11 @@ describe DashboardsController do
         get :show, session: {uni: 'fadsjkl'}
         expect(response).to redirect_to root_path
         expect(flash[:notice]).to match(/User not found. Please sign up first./)
+    end
+    it 'creates proper student schedule' do
+      get :show, session: {uni: 'agh2172', pw:'123456'}
+      sched = assigns(:schedule)
+      expect(sched[1][0].course).to include("COMS 4152 Engineering SaaS")
     end
     it 'updates student schedule succesfully (add)' do
         get :update, session: {uni: 'agh2172'}, params: {id: 4}
